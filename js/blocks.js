9bfcf3779e30e6084bad09a9f1853c1aa8536c12
Tid <yesseecity@yahoo.com.tw>
2020-01-13 18:11:15 +0800

    Add project files

---
+++ js/blocks.js
@@ -0,0 +1,23 @@
+Blockly.Blocks['my_block'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("自訂方塊");
+    this.appendDummyInput()
+        .appendField("下拉選單")
+        .appendField(new Blockly.FieldDropdown([["option","OPTIONNAME"], ["option","OPTIONNAME"], ["option","OPTIONNAME"]]), "d1");
+    this.appendValueInput("input")
+        .setCheck(null)
+        .appendField("由方塊輸入值");
+    this.setColour(230);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.JavaScript['my_block'] = function(block) {
+  var dropdown_d1 = block.getFieldValue('d1');
+  var value_input = Blockly.JavaScript.valueToCode(block, 'input', Blockly.JavaScript.ORDER_ATOMIC);
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
+  return code;
+};
---
+++ js/main.js
@@ -0,0 +1,35 @@
+var demoWorkspace = Blockly.inject('frame-for-blockly',{
+  toolbox: document.getElementById('toolbox'),
+  trashcan: true,
+  zoom:{
+    controls: true,
+    wheel: true,
+    startScale: 1.0,
+    maxScale: 3,
+    minScale: 0.3,
+    scaleSpeed: 1.2
+  }
+});
+
+function genCode () {
+  let code = Blockly.JavaScript.workspaceToCode(demoWorkspace);
+  document.getElementById('code').innerText = code;
+  localStorage.setItem('code', code);
+}
+
+function getXml() {
+  var xml = Blockly.Xml.workspaceToDom(demoWorkspace);
+  var xml_text = Blockly.Xml.domToPrettyText(xml);
+  localStorage.setItem('xml_text', xml_text);
+}
+
+(function loadLocalStorage() {
+  var xml_text = localStorage.getItem('xml_text');
+  var dom = Blockly.Xml.textToDom(xml_text);
+  Blockly.Xml.domToWorkspace(dom, demoWorkspace);
+
+
+  var code = localStorage.getItem('code');
+  console.log(code)
+  document.getElementById('code').innerText = code;
+})()
